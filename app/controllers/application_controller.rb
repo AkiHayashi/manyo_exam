181d324be8aa1194bcfaabff646dcf058f8ba771
aki hayashi <hayashi1019toy@gmail.com>
2021-08-27 16:32:42 +0900

    modify env file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['USER'], ENV['PASSWORD']
+  http_basic_authenticate_with name: ENV['USER'], ENV['PASS']
 end
