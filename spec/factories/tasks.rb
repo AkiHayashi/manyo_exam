131012f86ae7e9ae59351a676a1afd186ec61c73
aki hayashi <hayashi1019toy@gmail.com>
2021-09-02 01:16:36 +0900

    system and model spec done.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,5 +2,19 @@ FactoryBot.define do
   factory :task do
     title { 'test_title' }
     description { 'test_description' }
+    progress {'未着手'}
+    expired_at {'2021-08-30 00:00:00'}
+  end
+  factory :second_task, class: Task do
+    title { 'test_title2' }
+    description { 'test_description2' }
+    progress {'着手中'}
+    expired_at {'2021-08-30 00:00:00'}
+  end
+  factory :third_task, class: Task do
+    title { 'test_title3' }
+    description { 'test_description3' }
+    progress {'完了'}
+    expired_at {'2021-08-30 00:00:00'}
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 def basic_pass(path)
-  username = ENV["USER"] 
-  password = ENV["PASS"]
+  username = ENV["BASIC_AUTH_NAME"] 
+  password = ENV["BASIC_AUTH_PASSWORD"]
   visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
 end
 RSpec.describe 'タスク管理機能', type: :system do
   let!(:task) { 
-    FactoryBot.create(:task, title: 'タスク１')
-    FactoryBot.create(:task, title: 'タスク２')
-    FactoryBot.create(:task, title: 'タスク３') 
+    FactoryBot.create(:task, title: 'タスク１', progress: "未着手", priority:"低", expired_at: DateTime.new.strftime('2021-09-03 00:00:00'))
+    FactoryBot.create(:task, title: 'タスク２', progress: "着手中", priority:"中", expired_at: DateTime.new.strftime('2021-09-02 00:00:00'))
+    FactoryBot.create(:task, title: 'タスク３', progress: "完了", priority:"高", expired_at: DateTime.new.strftime('2021-09-01 00:00:00')) 
   }
   before do 
     basic_pass visit tasks_path
@@ -19,10 +19,13 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in "task[title]", with: "test task title"
         fill_in "task[description]", with: "test description"
+        select '着手中' , from: "task[progress]"
+        fill_in "task[expired_at]", with: DateTime.current.strftime("%Y\t%m%d%I%M%P")
         click_on "登録する"
         expect(page).to have_content "タスク作成完了"
         expect(page).to have_content "test task title"
         expect(page).to have_content "test description"
+        expect(page).to have_content "着手中"
       end
     end
   end
@@ -35,8 +38,53 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        task_list = all('.task_row').collect(&:text)
+        task_list = all('.task_row_created').collect(&:text)
         task_list[0] < task_list[1] || task_list[1] < task_list[2]
+      end
+    end
+
+    context 'タスクが終了期限の降順に並んでいる場合' do
+      it '終了期限の遅いタスクが一番上に表示される' do
+        find(".sort_expired_btn").click
+        task_list = all('.task_row_expired').collect(&:text)
+        expect(task_list[2]).to have_content "09/01"
+        task_list[0] > task_list[1] || task_list[1] > task_list[2]
+      end
+    end
+    context 'タスクが優先順位の高い順に並んでいる場合' do
+      it '優先順位の高いタスクが一番上に表示される' do
+        find(".sort_priority_btn").click
+        task_list = all('.task_row_priority').collect(&:text)
+        expect(task_list[0]).to have_content "高"
+      end
+    end
+  end
+  describe '検索機能' do
+    context '任意のタイトルで検索した場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        fill_in 'title', with: "タスク１"
+        click_on "検索"
+        expect(page).to have_content "タスク１"
+        expect(page).not_to have_content "タスク２" && "タスク３"
+      end
+    end
+    context '任意のステータスで検索した場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        select '着手中' , from: "progress"
+        click_on "検索"
+        expect(find('.task_row_progress')).to have_content "着手中"
+        expect(find('.task_row_progress')).not_to have_content "未着手" && "完了"
+      end
+    end
+    context '任意のタイトルとステータスの両方で検索した場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        fill_in 'title', with: "タスク１"
+        select '未着手' , from: "progress"
+        click_on "検索"
+        expect(page).to have_content "タスク１"
+        expect(page).not_to have_content "タスク２" && "タスク３"
+        expect(find('.task_row_progress')).to have_content "未着手"
+        expect(find('.task_row_progress')).not_to have_content "着手中" && "完了"
       end
     end
   end
